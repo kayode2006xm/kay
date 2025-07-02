b9ecfec5e777675d4d0f9e482af117535fa6f4dc
kayode2006xm <olowoesakayo@gmail.com>
2025-07-03 00:04:27 +0100

    Update and rename api train.py to ForexApiApplication.jsx

---
+++ ForexApiApplication.jsx
@@ -0,0 +1,11 @@
+package com.forexai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ForexApiApplication.class, args);
+    }
+}
